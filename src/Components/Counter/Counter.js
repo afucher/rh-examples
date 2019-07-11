077aff2ab0020ea8147e2ebaaeccf858e2ea2116
Arthur Fücher <arthurfucher@gmail.com>
2019-07-10 22:17:25 -0300

    added useEffect to the example

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import ThemeContext from '../../context';
 
-export default class Counter extends React.Component {
+class Counter extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            counter : 0,
-            increment : 1
-        };
+            counter: 0,
+            increment: 1
+        }
+        this.handleIncrement = this.handleIncrement.bind(this);
+        this.handleIncrementChange = this.handleIncrementChange.bind(this);
     }
 
-    handleClick = () => {
+    componentDidMount() {
+        window.document.title = this.state.counter;
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.counter === this.state.counter) return;
+        window.document.title = this.state.counter;
+    }
+
+    handleIncrement() {
         this.setState({
             counter: this.state.counter + this.state.increment
-        });
+        })
     }
 
-    handleIncrementChange = (e) => {
+    handleIncrementChange(e) {
         this.setState({
             increment: parseInt(e.target.value)
         })
@@ -26,15 +37,18 @@ export default class Counter extends React.Component {
     render() {
         return (
             <ThemeContext.Consumer>
-                {theme => (
+                {theme => 
                     <section className={theme}>
                         <p>{this.state.counter}</p>
-                        <button onClick={this.handleClick}>Increment</button>
-                        <input type="number" value={this.state.increment} onChange={this.handleIncrementChange}/>
+                        <button onClick={this.handleIncrement}>Increment</button>
+                        <input type="number" 
+                            value={this.state.increment}
+                            onChange={this.handleIncrementChange}/>
                     </section>
-                )}
-                
+                }
             </ThemeContext.Consumer>
         )
     }
 }
+
+export default Counter;
--- src/Hooks/Counter/Counter.js
+++ src/Hooks/Counter/Counter.js
@@ -1,29 +1,41 @@
-import React, { useState, useContext} from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import ThemeContext from '../../context';
 
 export const Counter = () => {
     let [increment, setIncrement] = useState(1);
     let [counter, incrementer] = useIncrementer(0,increment);
     let theme = useContext(ThemeContext);
+    
+    useEffect(() => {
+        window.document.title = counter;
+    },[counter])
+    
 
-    function handleIncrementChange(e) {
+    const handleIncrementChange = (e) => {
         setIncrement(parseInt(e.target.value));
     }
 
-    return <section className={theme}>
-        <p>{counter}</p>
-        <button onClick={incrementer}>Increment</button>
-        <input type="number" value={increment} onChange={handleIncrementChange}/>
-    </section>
+    return (
+        <section className={theme}>
+            <p>{counter}</p>
+            <button onClick={incrementer}>Increment</button>
+            <input type="number"
+                value={increment}
+                onChange={handleIncrementChange}/>
+        </section>
+    )
 }
 
+export default Counter;
 
+//---------------
 
+const useIncrementer = (initialValue, increment) => {
+    let [value, setValue] = useState(initialValue);
 
-const useIncrementer = (defaultValue, increment=1) => {
-    let [counter, setCounter] = useState(defaultValue);
+    const incrementer = () => {
+        setValue(value + increment);
+    }
 
-    const incrementer = () => setCounter(counter+increment);
-
-    return [counter, incrementer]
+    return [value, incrementer];
 }
