b24b2f4bbf4a809c341e8db17a49106199d4c045
Arthur Fücher <arthurfucher@gmail.com>
2019-07-10 21:10:36 -0300

    added router for each component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Counter from './Components/Counter/Counter'
 import {Counter as CounterHooks} from './Hooks/Counter/Counter'
 import './App.css';
@@ -6,14 +7,26 @@ import ThemeContext from './context';
 
 function App() {
   return (
-    <div className="App">
-      <ThemeContext.Provider value="blue">
-        <Counter />
-      </ThemeContext.Provider>
-      <ThemeContext.Provider value="yellow">
-        <CounterHooks />
-      </ThemeContext.Provider>
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" exact render={() => {
+          return <>
+            <ThemeContext.Provider value="blue">
+              <Counter />
+            </ThemeContext.Provider>
+            <ThemeContext.Provider value="yellow">
+              <CounterHooks />
+            </ThemeContext.Provider>
+          </>
+        }} />
+        <ThemeContext.Provider value="blue">
+          <Route path="/class" component={Counter} />
+        </ThemeContext.Provider>
+        <ThemeContext.Provider value="yellow">
+          <Route path="/function" component={CounterHooks} />
+        </ThemeContext.Provider>
+      </div>
+    </Router>
   );
 }
 
